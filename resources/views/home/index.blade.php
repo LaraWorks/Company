1a852f4852db9ee8bf802ded10ee8173c22bd8d9
Rayiumir <swordart2050@gmail.com>
2025-03-05 02:06:36 +0330

    Create A Slider

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -34,4 +34,29 @@
         </div>
     </nav>
     {{-- End Header--}}
+    {{-- Slider --}}
+    <section class="mt-3 mb-3">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('img/1.jpg') }}" class="d-block w-100 rounded-3" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('img/2.jpg') }}" class="d-block w-100 rounded-3" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('img/3.jpg') }}" class="d-block w-100 rounded-3" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+    {{-- End Slider--}}
 </x-HomeLayout>
