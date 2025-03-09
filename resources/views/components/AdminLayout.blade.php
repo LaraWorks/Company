35958b6c232fa816b1e549693ab5a7c04d8d9673
Rayiumir <swordart2050@gmail.com>
2025-03-10 01:47:06 +0330

    Created Admin with AdminRanger

---
+++ resources/views/components/AdminLayout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ادمین رنجر</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.rtl.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/style.css') }}">
+</head>
+<body>
+<div class="d-flex" id="wrapper">
+
+    @include('admin.inc.sidebar')
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+        @include('admin.inc.navbar')
+
+        <div class="container">
+            {{ $slot }}
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+</div>
+<!-- /#wrapper -->
+
+<!-- Script -->
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::get('/', function () {
 });
 
 Route::get('/', [\App\Http\Controllers\Home\HomeController::class, 'index']);
+
+
+Route::group(['prefix' => 'admin'], static function ($router) {
+
+    //Admin
+
+    $router->resource('/', \App\Http\Controllers\Admin\AdminController::class);
+});
